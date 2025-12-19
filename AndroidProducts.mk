66414e1a4fc36ef7cbaa50e7c47482dfbfc39891
robloxguyscripts-jpg <robloxguyscripts@gmail.com>
2025-12-19 18:30:55 -0500

    Remove copyright and license comments
    Removed copyright and license comments from AndroidProducts.mk.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,3 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_YT-X705X.mk
 
