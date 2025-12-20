6b9eb8fd71728a70faa829ea4d572163f2265cb5
robloxguyscripts-jpg <robloxguyscripts@gmail.com>
2025-12-19 22:20:20 -0500

    Update twrp_YT_X705X.mk

--- twrp_YT_X705X.mk
+++ twrp_YT_X705X.mk
@@ -2,22 +2,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit TWRP common configuration (Required for AOSP manifest)
+# Inherit TWRP common configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from the device configuration
-# Ensure this folder name matches exactly where you clone the repo
-$(call inherit-product, device/lenovo/android_device_lenovo_YT-X705X/device.mk)
+# Match this path to the 'Clone Device Tree' step in your YAML
+$(call inherit-product, device/lenovo/YT_X705X/device.mk)
 
 PRODUCT_DEVICE := YT_X705X
 PRODUCT_NAME := twrp_YT_X705X
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo YT-X705X
 PRODUCT_MANUFACTURER := lenovo
-
-PRODUCT_GMS_CLIENTID_BASE := android-lenovo-rev2
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="YT-X705X-user 10 QKQ1.191224.003 X705X_S001137_220802_ROW release-keys"
-
-BUILD_FINGERPRINT := Lenovo/YT-X705X/YT-X705X:10/QKQ1.191224.003/X705X_S001137_220802_ROW:user/release-keys
