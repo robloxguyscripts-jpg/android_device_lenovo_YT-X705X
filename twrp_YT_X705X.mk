7487227f36339c7860347bc2737b59b921b8a842
robloxguyscripts-jpg <robloxguyscripts@gmail.com>
2025-12-19 23:12:52 -0500

    Update twrp_YT_X705X.mk

--- twrp_YT_X705X.mk
+++ twrp_YT_X705X.mk
@@ -3,13 +3,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit TWRP common configuration
+# Note: For TWRP 11, the path is usually vendor/twrp/config/common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Match this path to the 'Clone Device Tree' step in your YAML
+# Inherit from your device.mk (The file we just fixed)
 $(call inherit-product, device/lenovo/YT_X705X/device.mk)
 
+# Device identifier
 PRODUCT_DEVICE := YT_X705X
 PRODUCT_NAME := twrp_YT_X705X
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo YT-X705X
 PRODUCT_MANUFACTURER := lenovo
+
+# Release info
+PRODUCT_RELEASE_NAME := Lenovo YT-X705X
