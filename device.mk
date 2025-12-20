2a75ade7be6bf8e1a76df5067893b3b55b3dc179
robloxguyscripts-jpg <robloxguyscripts@gmail.com>
2025-12-20 18:39:09 -0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,11 +5,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Recovery tools
 PRODUCT_PACKAGES += \
     otafault \
-    cpio
+    cpio \
+    toybox
 
 PRODUCT_SOONG_NAMESPACES += \
     device/lenovo/YT_X705X
 
+# THE 99% FIX: Copying to system/etc avoids the "root/etc" symlink collision
 PRODUCT_COPY_FILES += \
     device/lenovo/YT_X705X/recovery.fstab:recovery/root/system/etc/recovery.fstab \
     device/lenovo/YT_X705X/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc
