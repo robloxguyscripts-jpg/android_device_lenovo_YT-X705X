698406805b9625f5e8566221d47b7bf175102d7f
robloxguyscripts-jpg <robloxguyscripts@gmail.com>
2025-12-20 11:37:08 -0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -11,7 +11,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Recovery tools
-
 PRODUCT_PACKAGES += \
     otafault \
     cpio
@@ -19,7 +18,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     device/lenovo/YT_X705X
 
-
 PRODUCT_COPY_FILES += \
-    device/lenovo/YT_X705X/recovery.fstab:recovery/root/system/etc/recovery.fstab \
+    device/lenovo/YT_X705X/recovery.fstab:recovery/root/etc/recovery.fstab \
     device/lenovo/YT_X705X/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc
