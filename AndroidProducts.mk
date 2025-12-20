218e2e7e62c1ba7d8da35f05ddd8bf90f023b9e4
robloxguyscripts-jpg <robloxguyscripts@gmail.com>
2025-12-19 19:02:59 -0500

    Fix product naming in AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_YT-X705X.mk
+    $(LOCAL_DIR)/omni_YT_X705X.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_YT-X705X-user \
-    omni_YT-X705X-userdebug \
-    omni_YT-X705X-eng
+    omni_YT_X705X-user \
+    omni_YT_X705X-userdebug \
+    omni_YT_X705X-eng
