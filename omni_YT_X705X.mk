629f8dc6dc4563c32e8bf4e0f1749b9632909342
robloxguyscripts-jpg <robloxguyscripts@gmail.com>
2025-12-19 20:59:16 -0500

    Update omni_YT_X705X.mk

--- omni_YT_X705X.mk
+++ omni_YT_X705X.mk
@@ -4,27 +4,21 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from device configuration
-$(call inherit-product, device/lenovo/YT_X705X/device.mk)
+$(call inherit-product, device/lenovo/android_device_lenovo_YT-X705X/device.mk)
 
-# Device identifier
 PRODUCT_DEVICE := YT_X705X
 PRODUCT_NAME := omni_YT_X705X
 PRODUCT_BRAND := Lenovo
-PRODUCT_MODEL := Yoga Smart Tab
+PRODUCT_MODEL := Lenovo YT-X705X
 PRODUCT_MANUFACTURER := lenovo
-PRODUCT_RELEASE_NAME := YT-X705X
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo-rev2
 
-# Fingerprint and build descriptions
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="YT-X705X-user 10 QKQ1.191224.003 X705X_S001137_220802_ROW release-keys"
 
