080465af88690e5fab28ba1ff2041993d3632f3c
robloxguyscripts-jpg <robloxguyscripts@gmail.com>
2025-12-19 19:10:10 -0500

    Add recovery tools and namespaces to device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,17 @@
 #
 
 LOCAL_PATH := device/lenovo/YT-X705X
+
+# Inherit from core AOSP products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Recovery tools
+PRODUCT_PACKAGES += \
+    otafault \
+    cpio \
+    fec
+
+# Ensure the build system sees the namespace defined in Android.bp
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
