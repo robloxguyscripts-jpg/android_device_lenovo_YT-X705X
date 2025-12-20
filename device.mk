407a39ad37cf3125e11deb92bbab7f3fe960a68f
robloxguyscripts-jpg <robloxguyscripts@gmail.com>
2025-12-20 18:37:23 -0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,11 +1,3 @@
-
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
