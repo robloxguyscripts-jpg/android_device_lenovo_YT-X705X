05b3528c3e5f0a63b5611bfcf46a64984c5905c5
robloxguyscripts-jpg <robloxguyscripts@gmail.com>
2025-12-19 20:53:33 -0500

    Update omni_YT_X705X.mk

--- omni_YT_X705X.mk
+++ omni_YT_X705X.mk
@@ -11,18 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# --- THE FIX: Point to the folder name created by the Workflow ---
+# Inherit from device configuration
 $(call inherit-product, device/lenovo/YT_X705X/device.mk)
 
-# Product naming - Must match the lunch target
+# Device identifier
 PRODUCT_DEVICE := YT_X705X
 PRODUCT_NAME := omni_YT_X705X
 PRODUCT_BRAND := Lenovo
-PRODUCT_MODEL := Lenovo YT-X705X
+PRODUCT_MODEL := Yoga Smart Tab
 PRODUCT_MANUFACTURER := lenovo
+PRODUCT_RELEASE_NAME := YT-X705X
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo-rev2
 
+# Fingerprint and build descriptions
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="YT-X705X-user 10 QKQ1.191224.003 X705X_S001137_220802_ROW release-keys"
 
