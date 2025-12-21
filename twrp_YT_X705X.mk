d4066f949dbda6a1517274eda7aa202e18c8d422
robloxguyscripts-jpg <robloxguyscripts@gmail.com>
2025-12-20 19:31:38 -0500

    Update twrp_YT_X705X.mk

--- twrp_YT_X705X.mk
+++ twrp_YT_X705X.mk
@@ -1,11 +1,19 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit TWRP common configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Note: In TWRP 11, this usually points to the omni config base
+$(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from our fixed device.mk
+# Inherit from our fixed device.mk (This contains the 99% symlink fix)
 $(call inherit-product, device/lenovo/YT_X705X/device.mk)
 
 # Device identifier
@@ -16,3 +24,6 @@ PRODUCT_MODEL := Lenovo-YT-X705X
 PRODUCT_MANUFACTURER := lenovo
 
 PRODUCT_RELEASE_NAME := Lenovo-YT-X705X
+
+# Target Fingerprint (Matching a stable Android 10 build for this device)
+PRODUCT_GMS_CLIENTID_BASE := android-lenovo
