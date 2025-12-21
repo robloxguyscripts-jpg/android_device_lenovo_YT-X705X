ed07fe4a912517a929870bfa08fcabdd32598d6d
robloxguyscripts-jpg <robloxguyscripts@gmail.com>
2025-12-20 19:48:48 -0500

    Update twrp_YT_X705X.mk

--- twrp_YT_X705X.mk
+++ twrp_YT_X705X.mk
@@ -9,11 +9,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit TWRP common configuration
-# Note: In TWRP 11, this usually points to the omni config base
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit TWRP common configuration (AOSP Manifest Path)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from our fixed device.mk (This contains the 99% symlink fix)
+# Inherit from our device.mk
 $(call inherit-product, device/lenovo/YT_X705X/device.mk)
 
 # Device identifier
@@ -24,6 +23,3 @@ PRODUCT_MODEL := Lenovo-YT-X705X
 PRODUCT_MANUFACTURER := lenovo
 
 PRODUCT_RELEASE_NAME := Lenovo-YT-X705X
-
-# Target Fingerprint (Matching a stable Android 10 build for this device)
-PRODUCT_GMS_CLIENTID_BASE := android-lenovo
